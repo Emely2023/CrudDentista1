504e75c0d313941e01c2b23504f4f77d37374cf1
Emely <20230621@ricaldone.edu.sv>
2024-09-24 14:23:43 -0600

    tabla

---
+++ tbDentista.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tbDentista(
+UUID_Dentista VARCHAR2(100),
+NombreDentista VARCHAR2(100),
+EdadDentista INT,
+PesoDentista float,
+CorreoDentista VARCHAR2(100)
+);
+
+select * from tbDentista;
